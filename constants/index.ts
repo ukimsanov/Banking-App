4798f3d65e777cbfbaa70e6e3d901586beab3f21
rowflynn <rflynn@arcadia.edu>
2024-12-04 12:56:40 -0500

    delete unused python backend add budget page

--- constants/index.ts
+++ constants/index.ts
@@ -19,6 +19,11 @@ export const sidebarLinks = [
     route: "/payment-transfer",
     label: "Transfer Funds",
   },
+  {
+    imgURL: "/icons/money-send.svg",
+    route: "/budget",
+    label: "Budget",
+  },
 ];
 
 // good_user / good_password - Bank of America
