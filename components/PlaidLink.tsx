a5def90ae7b2aa9765e4fc7949057ff4d39c0f22
rowflynn <rflynn@arcadia.edu>
2024-12-05 19:46:53 -0500

    Implement banks page, transactions page. update BankCard, add functionality to add multiple banks, Add copy component

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react'
+import Image from 'next/image'
 import { Button } from './ui/button'
 import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from 'react-plaid-link'
 import { useRouter } from 'next/navigation';
@@ -45,12 +46,28 @@ const PlaidLink = ({ user, variant}: PlaidLinkProps) => {
             Connect bank
         </Button>
     ) : variant === "ghost" ? (
-        <Button>
-            Connect bank
+        <Button onClick={()=> open()} variant="ghost"
+        className="plaidlink-ghost">
+            <Image
+                src="/icons/connect-bank.svg"
+                alt="connect bank"
+                width={24}
+                height={24}
+            />
+            <p className="hiddenl text-[16px] font-semibold 
+            text-black-2 xl:block">Connect bank</p>
         </Button>
     ) : (
-        <Button>
-            Connect bank
+        <Button onClick={()=> open()}
+        className="plaidlink-default">
+            <Image
+                src="/icons/connect-bank.svg"
+                alt="connect bank"
+                width={24}
+                height={24}
+            />
+            <p className="text-[16px] font-semibold 
+            text-black-2">Connect bank</p>
         </Button>
     )}
     </>
