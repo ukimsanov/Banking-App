1e38329d265a9bb2db4de3f6c77e29af469d42ff
rowflynn <rflynn@arcadia.edu>
2024-12-06 18:45:26 -0500

    define new progress prop

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -5,9 +5,13 @@ import * as ProgressPrimitive from "@radix-ui/react-progress"
 
 import { cn } from "@/lib/utils"
 
+interface ProgressProps extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  indicatorClassName?: string;
+}
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+  ProgressProps
 >(({ indicatorClassName, className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
@@ -15,6 +19,7 @@ const Progress = React.forwardRef<
       "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
       className
     )}
+    value={value}
     {...props}
   >
     <ProgressPrimitive.Indicator
