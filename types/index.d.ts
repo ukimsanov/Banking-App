4a5660ac7c6098a4d963f06ba78dd3a6ada03d65
rowflynn <rflynn@arcadia.edu>
2024-12-06 18:07:23 -0500

    declare progress prop

--- types/index.d.ts
+++ types/index.d.ts
@@ -328,3 +328,10 @@ declare interface getBankProps {
 declare interface getBankByAccountIdProps {
   accountId: string;
 }
+
+declare interface ProgressProps extends PrimitiveDivProps {
+  indicatorClassName?: string;
+  value?: number | null | undefined;
+  max?: number;
+  getValueLabel?(value: number, max: number): string;
+}
