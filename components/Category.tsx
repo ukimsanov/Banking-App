ca119218049a2d1ea0fb0e265e4dc896aef25a31
rowflynn <rflynn@arcadia.edu>
2024-12-06 21:37:35 -0500

    remove customprogress and indicator classname

--- components/Category.tsx
+++ components/Category.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 import { topCategoryStyles } from "@/constants";
 import { cn } from "@/lib/utils";
 
-// Import CustomProgress without aliasing
-import { CustomProgress } from "./ui/CustomProgress";
+import { Progress } from "./ui/progress";
 
 const Category = ({ category }: CategoryProps) => {
   const {
@@ -14,7 +13,7 @@ const Category = ({ category }: CategoryProps) => {
     progress: { bg: progressBg, indicator },
     icon,
   } = topCategoryStyles[category.name as keyof typeof topCategoryStyles] ||
-    topCategoryStyles.default;
+  topCategoryStyles.default;
 
   return (
     <div className={cn("gap-[18px] flex p-4 rounded-xl", bg)}>
@@ -26,10 +25,10 @@ const Category = ({ category }: CategoryProps) => {
           <h2 className={cn("font-medium", main)}>{category.name}</h2>
           <h3 className={cn("font-normal", count)}>{category.count}</h3>
         </div>
-        <CustomProgress
+        <Progress
           value={(category.count / category.totalCount) * 100}
           className={cn("h-2 w-full", progressBg)}
-          indicatorClassName={cn("h-2 w-full", indicator)}
+          //indicatorClassName={cn("h-2 w-full", indicator)}
         />
       </div>
     </div>
