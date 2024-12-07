b96c7efec42c6093dcb6906602b3e0d6c0568bbb
rowflynn <rflynn@arcadia.edu>
2024-12-06 23:03:39 -0500

    remove commented duplicate code

--- constants/index.ts
+++ constants/index.ts
@@ -24,9 +24,6 @@ export const sidebarLinks = [
 // good_user / good_password - Bank of America
 export const TEST_USER_ID = "6627ed3d00267aa6fa3e";
 
-// custom_user -> Chase Bank
-// export const TEST_ACCESS_TOKEN =
-//   "access-sandbox-da44dac8-7d31-4f66-ab36-2238d63a3017";
 
 // custom_user -> Chase Bank
 export const TEST_ACCESS_TOKEN =
